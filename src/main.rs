17c449cfdec770d1dfa4d0c7b190b35e8f17f77e
manzogi9 <manzogi9@posteitaliane.it>
2025-10-23 14:27:32 +0200

    Reactor::dispatch

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
+use std::error::Error;
 use std::ffi::OsStr;
-use std::io::Write;
+use std::io::{Error as IoError, Write};
 
 use interprocess::local_socket::{Listener, *};
 use interprocess::local_socket::traits::Listener as Listen;
 
+use std::io::Result;
+
 enum Request {
-    Accept,
-    Read,
+    Log(String)
+}
+
+impl Request {
+    fn log(s:&str) -> Option<Request> {
+        Some( Request::Log(String::from(s)) )
+    }
 }
 
 struct Reactor {
@@ -21,16 +29,30 @@ impl Reactor {
     }
 
     fn accept<F>(&mut self, ls:&mut Listener, hn:F)
-        where F: Fn(Stream) -> Option<Request>
+        where F: Fn(Result<Stream>) -> Option<Request>
     {
 
         match ls.accept() {
             Ok(stream) => {
-                if let Some(req) = hn(stream) {
+                if let Some(req) = hn( Ok(stream) ) {
                     self.q.push( req );
                 }
             },
-            Err(_) => todo!(),
+            err => {
+                if let Some(req) = hn( err ) {
+                    self.q.push( req );
+                }
+            },
+        }
+
+    }
+
+    fn dispatch(&mut self) {
+
+        if let Some(req) = self.q.pop() {
+            match req {
+                Request::Log(buf) => println!("Log: {buf}"),
+            }
         }
 
     }
@@ -43,13 +65,19 @@ fn main() {
         .nonblocking(ListenerNonblockingMode::Stream)
         .name( OsStr::new("ThePipe").to_ns_name::<GenericNamespaced>().unwrap() );
     let mut pipe = option.create_sync().unwrap();
-    
+
     let mut reactor = Reactor::new();
-    reactor.accept(&mut pipe, |mut stream:Stream|{
-        println!("Client accepted");
-        stream.write_all(b"Hello\n").unwrap();
-        None
+    reactor.accept(&mut pipe, |res:Result<Stream>|{
+        match res {
+            Ok(mut stream) => {
+                stream.write_all( b"Ciaone\n" ).unwrap();
+                Request::log("Client says bye!")
+            },
+            Err(err) => Request::log(err.description()),
+        }
     });
+
+    reactor.dispatch();
 
     // wait for event (block)
     // dispatch event to proper handler
